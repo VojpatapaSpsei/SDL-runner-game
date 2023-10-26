e56089bc6bb7ba65f8f18cd5fb747471bc89e569
Vojpatapa <v.senkerik.st@spseiostrava.cz>
2023-10-26 17:56:36 +0200

    FPS calculator
    Added FPS regulator and counter.

--- main.cpp
+++ main.cpp
@@ -4,10 +4,19 @@ Game * game;
 
 int WinMain(int argc, char * argv[])
 {
-    game = new Game();
+    game = new Game("Runner", 1440, 810);
+
+    const int FPS = 60;
+    Uint32 start;
+    int delay = 1000/FPS;
+    int time;
+    int frametime;
+    int count;
 
     while(game->runnin())
     {
+        start = SDL_GetTicks();
+
         game->handleEvents();
 
         if(game->state == game->welcome)
@@ -30,7 +39,24 @@ int WinMain(int argc, char * argv[])
             game->update_end();
             game->render_end();
         }
+
+        count++;
+        time = SDL_GetTicks() - start;
+        if(delay>time)
+        {
+            SDL_Delay(delay-time);
+        }
+
+        frametime+=SDL_GetTicks()-start;
+        if(frametime>=1000)
+        {
+            printf("%d \n", count);
+            frametime = 0;
+            count=0;
+        }
+
     }
+
     delete game;
 
     return 0;
